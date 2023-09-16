55865ae9c82d51900fe266a1f328ad5ef231628e
greatxrider <daligdigjeph09@gmail.com>
2023-09-17 02:21:42 +0800

    finish part 1 of project

--- server/djangoapp/urls.py
+++ server/djangoapp/urls.py
@@ -10,8 +10,12 @@ urlpatterns = [
     # name the URL
 
     # path for about view
+     
+    path(route = 'djangoapp/about', view = views.about, name = 'about'),
 
     # path for contact us view
+    
+    path(route = 'djangoapp/contact', view = views.contact, name = 'contact'),
 
     # path for registration
 
@@ -25,4 +29,4 @@ urlpatterns = [
 
     # path for add a review view
 
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+] + static(settings.STATIC_URL, document_root=settings.STATICFILES_DIRS)
--- server/djangobackend/settings.py
+++ server/djangobackend/settings.py
@@ -13,7 +13,7 @@ import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 
 # Quick-start development settings - unsuitable for production
@@ -27,7 +27,7 @@ DEBUG = True
 
 APPEND_SLASH = True
 
-ALLOWED_HOSTS = ["localhost"]
+ALLOWED_HOSTS = ["localhost", "127.0.0.1"]
 
 
 # Application definition
@@ -80,7 +80,7 @@ WSGI_APPLICATION = 'djangobackend.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
 
